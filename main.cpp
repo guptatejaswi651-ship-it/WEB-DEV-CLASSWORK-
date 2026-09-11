eeaf4fc2cac86af72b18a4e7eee16772f5558c90
Tejaswi <guptatejaswi651@gmail.com>
2026-09-11 12:15:34 +0530

    my i doing

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    //Datatype variablename =value;
+   
+    int a=10;
+    int b=20;
+    int sum=a+b;
+    cout<<"The sum of a and b "<<sum<<endl;
+
+    return 0;
+    
+
+}
+
+
